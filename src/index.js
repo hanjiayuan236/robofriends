2767f65b0ffd2f7fcdef1337c6473bb19fcae674
Jiayuan <jh102@rice.edu>
2020-11-08 21:46:49 -0600

    first upload

--- src/index.js
+++ src/index.js
@@ -1,12 +1,15 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
+import App from './containers/App';
+import CardList from './components/CardList';
 import reportWebVitals from './reportWebVitals';
+import 'tachyons'
+// import { robots } from './components/robots';
+import './index.css';
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+  	<App/>
   </React.StrictMode>,
   document.getElementById('root')
 );
